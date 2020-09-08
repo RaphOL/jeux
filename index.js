993fea0d35767b95d9d7ecd0c54818a18a0a5464
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-08 13:47:39 +0200

    début de la création des manches

--- index.js
+++ index.js
@@ -2,45 +2,60 @@ const buttons = document.querySelectorAll("button");
 const equality = document.getElementById("equality");
 const win = document.getElementById("win");
 const lose = document.getElementById("lose");
+const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
+const imagePlayer = document.getElementById("a");
+const imageRobot = document.getElementById("b");
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        // console.log("ça marche");
-        const player = buttons[i].innerHTML;
-        const imagePlayer = document.getElementById("a");
-        const imageRobot = document.getElementById("b");
+window.onload = setButton();
+//logique de deuxième manche
+//lancerDeuxiemeManche() {
+//    setButton();
+//}
 
-        if (player === "Rock") {
-            imagePlayer.setAttribute("src", "./rock.png");
-        } else if (player === "Paper") {
-            imagePlayer.setAttribute("src", "./paper.png");
-        } else if (player === "Scissors") {
-            imagePlayer.setAttribute("src", "./scissors.png");
-        }
-        // console.log(player);
-        const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
+function setButton() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            const player = buttons[i].innerHTML;
+            setImagePlayer(player, imagePlayer);
+            setImageRobot(robot, imageRobot);
+            setResult(player, robot);
+        });
+    }
+}
 
-        if (robot === "Rock") {
-            imageRobot.setAttribute("src", "./rock.png");
-        } else if (robot === "Paper") {
-            imageRobot.setAttribute("src", "./paper.png");
-        } else if (robot === "Scissors") {
-            imageRobot.setAttribute("src", "./scissors.png");
-        }
+function setImagePlayer(player, imagePlayer) {
+    if (player === "Rock") {
+        imagePlayer.setAttribute("src", "./rock.png");
+    } else if (player === "Paper") {
+        imagePlayer.setAttribute("src", "./paper.png");
+    } else if (player === "Scissors") {
+        imagePlayer.setAttribute("src", "./scissors.png");
+    }
+}
 
-        let result = "";
-        if (player === robot) {
-            result = "Equality";
-            equality.play();
-        } else if ((player === "Rock" && robot === "Scissors") || (player === "Paper" && robot === "Rock") || (player === "Scissors" && robot === "Paper")) {
-            result = "You Win!";
-            win.play();
-        } else {
-            result = "You Loooooose!";
-            lose.play();
-        }
+function setImageRobot(robot, imageRobot) {
+    if (robot === "Rock") {
+        imageRobot.setAttribute("src", "./rock.png");
+    } else if (robot === "Paper") {
+        imageRobot.setAttribute("src", "./paper.png");
+    } else if (robot === "Scissors") {
+        imageRobot.setAttribute("src", "./scissors.png");
+    }
+}
 
-        document.querySelector(".result").innerHTML =
-            `${result}`;
-    });
+function setResult(player, robot) {
+    let result = "";
+    if (player === robot) {
+        result = "Equality";
+        // equality.play();
+    } else if ((player === "Rock" && robot === "Scissors") || (player === "Paper" && robot === "Rock") || (player === "Scissors" && robot === "Paper")) {
+        result = "You Win!";
+        // win.play();
+    } else {
+        result = "You Loooooose!";
+        // lose.play();
+    }
+
+    document.querySelector("#resultContainer").innerHTML =
+        `<div class="result">${result}</div>`;
 }
